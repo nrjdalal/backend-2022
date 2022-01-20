46fb24dd478e54b9c0253fc7f773aaae78869d6c
Neeraj Dalal <admin@nrjdalal.com>
2022-01-20 21:15:45 +0530

    minor updates

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,4 +5,8 @@ const router = express.Router()
 import register from '../controllers/user/register.js'
 router.post('/register', register)
 
+router.get('/', (req, res) => {
+  res.send('VeroXyle')
+})
+
 export default router
