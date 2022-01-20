c55c07360e70f5ade915c950e0886072f0508e83
Neeraj Dalal <admin@nrjdalal.com>
2022-01-20 19:48:22 +0530

    minor updates

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,4 +5,10 @@ const router = express.Router()
 import register from '../controllers/user/register.js'
 router.post('/register', register)
 
+// ---
+router.get('/', (req, res) => {
+  res.send('Node server is running on vercel?')
+})
+// ---
+
 export default router
